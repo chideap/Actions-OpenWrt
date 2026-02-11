678bf893044df43b5bb4ca0337e9224c06a70ac8
chideap <40379524+chideap@users.noreply.github.com>
2026-02-11 14:25:15 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -5,7 +5,7 @@
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
 # mode
-echo 'src-git QModem https://github.com/FUjr/QModem' >> feeds.conf.default
+# echo 'src-git QModem https://github.com/FUjr/QModem' >> feeds.conf.default
 
 # OpenClash
 git clone --depth 1 https://github.com/vernesong/OpenClash.git OpenClash
